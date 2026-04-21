b45dca4bcef675d0ab590f07dcef5a79a42e4130
Felix Andersson <felix.andersson@gmail.com>
2026-04-21 20:24:21 +0200

    part 1 complete

--- src/main/java/se/yrgo/client/SimpleClient.java
+++ src/main/java/se/yrgo/client/SimpleClient.java
@@ -1,10 +1,27 @@
 package se.yrgo.client;
 
+import java.util.List;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import se.yrgo.domain.Customer;
+import se.yrgo.services.customers.CustomerManagementService;
 
 public class SimpleClient {
 
     public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext container = new ClassPathXmlApplicationContext("application.xml");
+
+        CustomerManagementService service =
+                container.getBean("customerManagement", CustomerManagementService.class);
+
+        List<Customer> customers = service.getAllCustomers();
+
+        for (Customer customer : customers) {
+            System.out.println(customer);
+        }
+
+        container.close();
 
     }
 }
