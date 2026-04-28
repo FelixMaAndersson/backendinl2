a6af0dc332326b8f8fa0378fcd2d09c007409b63
Felix Andersson <felix.andersson@gmail.com>
2026-04-28 18:04:44 +0200

    part 1 done

---
+++ src/main/java/se/yrgo/advice/PerformanceTimingAdvice.java
@@ -0,0 +1,31 @@
+package se.yrgo.advice;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class PerformanceTimingAdvice {
+    @Around("execution(* se.yrgo.services.*.*(..)) || execution(* se.yrgo.dataaccess.*.*(..))")
+    public Object performTimingMeasurement(ProceedingJoinPoint pjp) throws Throwable {
+
+        Long start = System.nanoTime();
+
+        Object result = pjp.proceed();
+
+        Long end = System.nanoTime();
+
+        double time = (end - start) / 1_000_000.0;
+
+        String methodName = pjp.getSignature().getName();
+
+        String className = pjp.getSignature().getDeclaringTypeName();
+
+        System.out.println("Time taken for the method " + methodName + " from the class " + className + " took " + time + "ms");
+
+        return result;
+    }
+}
